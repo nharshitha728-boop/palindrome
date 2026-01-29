df61ceb5251c31efe438b84b1f3988ef95a1cc9a
Harshitha <nharshitha728@gmail.com>
2026-01-29 22:33:59 +0530

    Update Palindrome.java

--- Palindrome.java
+++ Palindrome.java
@@ -1,6 +1,6 @@
 public class Palindrome {
     public static void main(String[] args) {
-        String s = "hello";
+        String s = "madam";
         String rev = "";
 
         for (int i = s.length() - 1; i >= 0; i--) {
